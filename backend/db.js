11a349c246bc1e8d56047f03ec1589943e335467
unknown <abhinavkb2004@gmail.com>
2025-06-09 16:25:12 +0530

    clear

--- backend/db.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-
-
-mongoose.connect("mongodb+srv://killerbean:Password@cluster0.sddlmmp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-.then(() =>{console.log("DB_CONNECTED")})
-.catch((err)=>{console.log(err)})
